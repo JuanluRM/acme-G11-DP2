bc9298961f51bfe5903c79afc86871252272f870
JuanluRM <juanluis.ruano.muriedas@gmail.com>
2023-03-02 12:32:27 +0100

    Modelo individual y clase Note

---
+++ Acme-L3-D01-23.1.0/src/main/java/acme/entities/Note.java
@@ -0,0 +1,61 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+/*
+ * @pattern(reg)
+ */
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Getter
+@Setter
+public class Note extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes --------------------------------------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotBlank
+	protected Date				instantiationMoment;
+
+	@NotBlank
+	@Length(min = 1, max = 76)
+	protected String			title;
+
+	@NotBlank
+	@Length(min = 1, max = 76)
+	protected String			author;
+
+	@NotBlank
+	@Length(min = 1, max = 101)
+	protected String			message;
+
+	@Email
+	protected String			email;
+
+	@URL
+	protected String			link;
+
+}
