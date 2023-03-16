d76d5a0b28314e3c4ea862d7a183da9cf696dc63
marruivaz1 <marruivaz1@alumn.us.es>
2023-03-16 20:49:34 +0100

    datos añadido

--- Acme-L3-D01-23.1.0/src/main/java/form/AuditorDashboard.java
+++ Acme-L3-D01-23.1.0/src/main/java/form/AuditorDashboard.java
@@ -2,13 +2,16 @@
 package form;
 
 import java.io.Serializable;
+import java.util.Map;
+
+import acme.entities.SessionType;
 
 public class AuditorDashboard implements Serializable {
 
 	protected static final long	serialVersionUID	= 1L;
 
-	//Map<SessionType, Integer>	totalNumAuditsForTheoryCourses;
-	//Map<SessionType, Integer>	totalNumAuditsForHandsOnCourses;
+	Map<SessionType, Integer>	totalNumAuditsForTheoryCourses;
+	Map<SessionType, Integer>	totalNumAuditsForHandsOnCourses;
 
 	Double						averageAudits;
 	Double						deviationAudits;
