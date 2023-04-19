5d557e9535ee0d7750f46c523e0e814dc52d7d30
pabquide <48489345+pabquide@users.noreply.github.com>
2023-04-19 22:42:20 +0200

    Falta que funcione el create
    :-1:

---
+++ Acme-L3-D01-23.1.0/src/main/java/acme/features/student/StudentEnrolmentListService.java
@@ -0,0 +1,52 @@
+
+package acme.features.student;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.Enrolment;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+import acme.roles.Student;
+
+@Service
+public class StudentEnrolmentListService extends AbstractService<Student, Enrolment> {
+
+	@Autowired
+	StudentEnrolmentRepository repository;
+
+
+	@Override
+	public void check() {
+		super.getResponse().setChecked(true);
+	}
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		final Collection<Enrolment> objects;
+		int studentId;
+
+		studentId = super.getRequest().getPrincipal().getActiveRoleId();
+		objects = this.repository.findEnrolmentsByStudentId(studentId);
+
+		super.getBuffer().setData(objects);
+	}
+
+	@Override
+	public void unbind(final Enrolment object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "code", "motivation", "goals", "workTime");
+
+		super.getResponse().setData(tuple);
+	}
+}
---
+++ Acme-L3-D01-23.1.0/src/main/java/acme/features/student/StudentEnrolmentUpdateService.java
@@ -0,0 +1,76 @@
+
+package acme.features.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.Enrolment;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+import acme.roles.Student;
+
+@Service
+public class StudentEnrolmentUpdateService extends AbstractService<Student, Enrolment> {
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected StudentEnrolmentRepository repository;
+
+	// AbstractService<Employer, Company> -------------------------------------
+
+
+	@Override
+	public void check() {
+		boolean status;
+
+		status = super.getRequest().hasData("id", int.class);
+
+		super.getResponse().setChecked(status);
+	}
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Enrolment object;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findOneEnrolmentById(id);
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void bind(final Enrolment object) {
+		assert object != null;
+
+		super.bind(object, "code", "motivation", "goals", "workTime");
+	}
+
+	@Override
+	public void validate(final Enrolment object) {
+		assert object != null;
+	}
+
+	@Override
+	public void perform(final Enrolment object) {
+		assert object != null;
+
+		this.repository.save(object);
+	}
+
+	@Override
+	public void unbind(final Enrolment object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "code", "motivation", "goals", "workTime");
+
+		super.getResponse().setData(tuple);
+	}
+}
